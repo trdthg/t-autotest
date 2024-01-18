ea6176104e427ad529e397827ae10e77f5b3e225
trdthg <trdthg47@gmail.com>
2024-01-19 02:16:06 +0800

    fix cargo test

--- crates/t-cli/src/needle.rs
+++ crates/t-cli/src/needle.rs
@@ -4,30 +4,24 @@ use serde::{Deserialize, Serialize};
 use t_console::{Rect, PNG};
 
 pub struct NeedleManager {
-    dir: String,
+    dir: PathBuf,
 }
 
 impl NeedleManager {
-    pub fn new(dir: impl Into<String>) -> Self {
+    pub fn new(dir: impl Into<PathBuf>) -> Self {
         Self { dir: dir.into() }
     }
 
     pub fn load_by_tag(&self, tag: &str) -> (NeedleConfig, PNG) {
-        let needle_path = PathBuf::from_iter(vec![&self.dir, &format!("{tag}.png")]);
-        let needle_file = File::open(needle_path).unwrap();
-        let needle_png = image::load(BufReader::new(needle_file), image::ImageFormat::Png).unwrap();
-        let needle_png = needle_png.into_rgb8();
-
-        let json_path = PathBuf::from_iter(vec![&self.dir, &format!("{tag}.json")]);
-        let json_file = File::open(json_path).unwrap();
+        let needle_png = self.load_file_by_tag(tag);
+        let json_file = File::open(self.dir.join(format!("{tag}.json"))).unwrap();
         let json: NeedleConfig = serde_json::from_reader(BufReader::new(json_file)).unwrap();
         (json, needle_png)
     }
 
-    #[cfg(test)]
     pub fn load_file_by_tag(&self, tag: &str) -> PNG {
-        let needle_path = PathBuf::from_iter(vec![&self.dir, &format!("{tag}.png")]);
-        let needle_file = File::open(needle_path).unwrap();
+        println!("{}", self.dir.join(format!("{tag}.png")).display());
+        let needle_file = File::open(self.dir.join(format!("{tag}.png"))).unwrap();
         let needle_png = image::load(BufReader::new(needle_file), image::ImageFormat::Png).unwrap();
 
         needle_png.into_rgb8()
@@ -111,14 +105,108 @@ impl From<&Area> for Rect {
 
 #[cfg(test)]
 mod test {
+    use std::fs;
+
     use super::NeedleManager;
-    use crate::needle::cmp_image_rect;
+    use crate::needle::{cmp_image_rect, Area, NeedleConfig};
+    use image::{ImageBuffer, Rgb};
     use t_console::Rect;
+
+    fn init_needle_manager() -> NeedleManager {
+        // 创建临时文件夹
+        let temp_dir = std::env::temp_dir();
+        println!("{:?}", temp_dir);
+
+        let tmp_needle_folder = temp_dir.join("needle");
+        if fs::metadata(&tmp_needle_folder).is_ok() {
+            fs::remove_dir_all(&tmp_needle_folder).unwrap();
+        }
+        fs::create_dir(&tmp_needle_folder).unwrap();
+        let mut image_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(5, 5);
+        for pixel in image_buffer.pixels_mut() {
+            *pixel = Rgb([0, 0, 0]);
+        }
+
+        image_buffer
+            .save_with_format(
+                tmp_needle_folder.join("output.png"),
+                image::ImageFormat::Png,
+            )
+            .unwrap();
+        fs::write(
+            tmp_needle_folder.join("output.json"),
+            r#"
+            {
+                "area": [
+                    {
+                        "type": "match",
+                        "left": 0,
+                        "top": 0,
+                        "width": 5,
+                        "height": 5
+                    }
+                ],
+                "properties": [],
+                "tags": [
+                    "output"
+                ]
+            }
+        "#,
+        )
+        .unwrap();
+
+        // update middle pixel
+        image_buffer.put_pixel(2, 2, Rgb([255, 255, 255]));
+        image_buffer
+            .save_with_format(
+                tmp_needle_folder.join("output2.png"),
+                image::ImageFormat::Png,
+            )
+            .unwrap();
+        fs::write(
+            tmp_needle_folder.join("output2.json"),
+            r#"
+            {
+                "area": [
+                    {
+                        "type": "match",
+                        "left": 0,
+                        "top": 0,
+                        "width": 5,
+                        "height": 5
+                    }
+                ],
+                "properties": [],
+                "tags": [
+                    "output2"
+                ]
+            }
+        "#,
+        )
+        .unwrap();
+        NeedleManager::new(tmp_needle_folder)
+    }
 
     #[test]
     fn get_needle() {
-        let needle = NeedleManager::new("./assets/needles");
-        let (cfg, png) = needle.load_by_tag("normal2");
+        let needle = init_needle_manager();
+        let (cfg, png) = needle.load_by_tag("output");
+
+        assert_eq!(
+            cfg,
+            NeedleConfig {
+                area: vec![Area {
+                    type_field: "match".to_string(),
+                    left: 0,
+                    top: 0,
+                    width: 5,
+                    height: 5,
+                }],
+                properties: Vec::new(),
+                tags: vec!["output".to_string()]
+            }
+        );
+
         let rect = &cfg.area[0];
         let rect = Rect {
             left: rect.left,
@@ -126,10 +214,9 @@ mod test {
             width: rect.width,
             height: rect.height,
         };
-
         assert!(cmp_image_rect(&png, &png, &rect));
 
-        let png2 = needle.load_file_by_tag("normal");
+        let png2 = needle.load_file_by_tag("output2");
         assert!(!cmp_image_rect(&png, &png2, &rect));
     }
 }
